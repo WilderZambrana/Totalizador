421c9bf8223444eb8796506eb73898049720c328
Wilder Zambrana <jhonvalenz.wz@gmail.com>
2021-09-06 17:59:23 -0400

    Ingresar precio y mostrarlo en alert

--- src/totalizador.js
+++ src/totalizador.js
@@ -1,7 +1,10 @@
 const cantidad = document.querySelector("#cantidad-input");
+const precio = document.querySelector("#precio-input");
 const form = document.querySelector("#totalizador-form");
+
 
 form.addEventListener("submit", (event)=>{
     event.preventDefault();
-    alert("Cantidad: "+cantidad.value);
+    alert("Cantidad: "+cantidad.value+"\n"+"Precio: "+precio.value);
+    //alert("Precio: "+ precio.value);
 });
